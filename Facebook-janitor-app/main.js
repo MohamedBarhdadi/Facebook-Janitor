eda38444cb184acab06e7841eaf0d07ca9e0b9a0
Abdelrahman Abdelhafez <abdelrahman@maged.rocks>
2020-01-21 20:06:33 +0100

    :children_crossing: Enable keybindings on macOS

--- Facebook-janitor-app/main.js
+++ Facebook-janitor-app/main.js
@@ -46,6 +46,17 @@ function createWindow () {
           ]
         },
         {
+          label: "Edit",
+          submenu: [
+              { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+              { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+              { type: "separator" },
+              { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+              { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+              { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+              { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+          ]},
+        {
           label: 'Github',
           submenu:[
             {label: 'Github page',
